c708753f704d5812c05113b24418646e9d669350
LarsGJobloop <lars.gunnar@jobloop.no>
2024-09-03 11:04:57 +0200

    render out movie list from ghibli studios

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-console.log('Hello!');
+// console.log("Starting JavaScript")
+
+// while(true) {
+//     console.log("New Turn")
+//     await waitFor()
+// }
+
+// async function waitFor() {
+//     return new Promise((resolve) => {
+//         setTimeout(() => {resolve()}, 3000)
+//     })
+// }
+
+const apiUrl = "https://ghibliapi.vercel.app/films"
+
+const response = await fetch(apiUrl)
+const data = await response.json()
+
+const movieListElement = document.querySelector("#movie-list")
+console.log(movieListElement)
+
+for (const movie of data) {
+    console.log(movie)
+
+    const movieCard = document.createElement("li")
+    movieCard.textContent = movie.title
+
+    movieListElement.append(movieCard)
+}
